1f1b54a2342eb42e621dd614c0c1b47e773b7396
Albert Iskhakov <aliskhakov@gmail.com>
2015-09-18 11:51:42 +0300

    empty package added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ipython-redbeaver',
+    version='0.0.1',
+    packages=['ipython-redbeaver'],
+    url='https://github.com/aliskhakov/ipython-redbeaver',
+    license='Apache License 2.0',
+    author='Albert Iskhakov',
+    author_email='aliskhakov@gmail.com',
+    description=''
+)
